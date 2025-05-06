e8792e1b3694d51dc94234cdb224a42a1dd319a5
FellpsTavares <fellipet03@gmail.com>
2025-05-05 22:09:52 -0300

    Projeto funcionando com o front

--- projeto_doacao/core/urls.py
+++ projeto_doacao/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastrar-usuario/', views.cadastrar_usuario, name='cadastrar_usuario'),
-    path('cadastrar-local/', views.cadastrar_local, name='cadastrar_local'),
-    path('cadastrar-familia/', views.cadastrar_familia, name='cadastrar_familia'),
+    path('cadastrar_usuario/', views.cadastrar_usuario, name='cadastrar_usuario'),
+    path('cadastrar_local/', views.cadastrar_local, name='cadastrar_local'),
+    path('cadastrar_familia/', views.cadastrar_familia, name='cadastrar_familia'),
 ]
